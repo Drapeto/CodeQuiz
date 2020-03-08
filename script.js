fd1c2dff8a2f2c6d1504215d1c1b530950415abd
Drapeto <drapetorres@gmail.com>
2020-03-07 23:44:43 -0700

    Questions for JavaScript are complete

---
+++ script.js
@@ -0,0 +1,27 @@
+var questions = [
+{
+    question: "What is querySelector?",
+    options: ["button", "math", "object method", "array"],
+    answer: "object method"
+},
+{
+    question: "What does the acronym JSON mean?",
+    options: ["javaScript object number", "javaScript object node", "javaScript object notation"],
+    answer: "javaScript object notation"
+},
+{
+    question: "The Element Object represents and HTML element, like P, DIV, A, TABLE, or any other HTML element.",
+    options: ["True", "False"],
+    answer: "False"
+},
+{
+    question: "Which is an example of a variables?",
+    options: ["var price1 = 5;", "var x = 5;", "var pi = 3.14;", "All of the above"],
+    answer: "All of the above"
+},
+{
+    question: "Which is an example of a JavaScript Object converted into JSON?",
+    options: ["A. var myObj = { "":"", "":, "":"" };", "B. var myJSON = JSON.stringify(myObj);"],
+    answer: "var myJSON = JSON.stringify(myObj);"
+},
+]
